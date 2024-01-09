25558aea6180b60a587bba5e8bee8c198de2b3ea
nongyehong <2439646234@qq.com>
2024-01-10 04:51:15 +0800

    新增模块，完善配置

---
+++ HuLa-IM-framework/HuLa-IM-common-starter/src/main/java/com/HuLa/HuLaImCommonStarterApplication.java
@@ -0,0 +1,13 @@
+package com.HuLa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuLaImCommonStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuLaImCommonStarterApplication.class, args);
+    }
+
+}
--- HuLa-IM-service/src/main/java/com/HuLa/HuLaImServiceApplication.java
+++ HuLa-IM-service/src/main/java/com/HuLa/HuLaImServiceApplication.java
@@ -3,14 +3,11 @@ package com.HuLa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * @author nyh
- */
 @SpringBootApplication
-public class HuLaImServerApplication {
+public class HuLaImServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HuLaImServerApplication.class, args);
+        SpringApplication.run(HuLaImServiceApplication.class, args);
     }
 
 }
