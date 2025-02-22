9f569ed0b17ae55c2e5099a0a8cd41f60835c807
Dawn <2439646234@qq.com>
2025-02-22 21:01:18 +0800

    1.修复用户状态缓存问题

--- hula-im-service/src/main/java/com/hula/core/user/service/impl/UserStateServiceImpl.java
+++ hula-im-service/src/main/java/com/hula/core/user/service/impl/UserStateServiceImpl.java
@@ -9,6 +9,7 @@ import com.hula.core.user.domain.vo.resp.user.UserInfoResp;
 import com.hula.core.user.domain.vo.resp.user.UserStateVo;
 import com.hula.core.user.service.UserService;
 import com.hula.core.user.service.UserStateService;
+import com.hula.core.user.service.cache.UserCache;
 import com.hula.core.user.service.cache.UserSummaryCache;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -26,6 +27,7 @@ public class UserStateServiceImpl implements UserStateService {
 
     private UserStateDao userStateDao;
 	private UserSummaryCache userSummaryCache;
+	private UserCache userCache;
 	private UserService userService;
 	private PushService pushService;
 
@@ -47,6 +49,7 @@ public class UserStateServiceImpl implements UserStateService {
 
 		if (ObjectUtil.isNotNull(userState) && changeUserState){
 			// 1.清除缓存
+			userCache.userInfoChange(uid);
 			userSummaryCache.delete(uid);
 
 			// 2.推送数据
