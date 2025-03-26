519d76554a6e6fb41211bf4e969e1f0e5b5caf80
Dawn <2439646234@qq.com>
2025-03-26 17:05:26 +0800

    1.优化邮箱发送

--- hula-im-service/src/main/java/com/hula/core/user/service/impl/EmailServiceImpl.java
+++ hula-im-service/src/main/java/com/hula/core/user/service/impl/EmailServiceImpl.java
@@ -1,31 +1,39 @@
 package com.hula.core.user.service.impl;
 
+import cn.hutool.core.date.LocalDateTimeUtil;
 import cn.hutool.core.util.StrUtil;
-import com.hula.common.utils.ToolsUtil;
 import com.hula.core.user.dao.UserDao;
 import com.hula.core.user.domain.vo.req.user.BindEmailReq;
+import com.hula.core.user.service.ConfigService;
 import com.hula.core.user.service.EmailService;
 import com.hula.exception.BizException;
 import com.hula.utils.RedisUtils;
-import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.annotation.Resource;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 /**
  * 邮箱服务
  */
 @Service
 public class EmailServiceImpl implements EmailService {
-    @Autowired
-    private JavaMailSender mailSender;
+	@Resource
+	private JavaMailSender mailSender;
 
-	@Autowired
+	@Resource
 	private UserDao userDao;
-    
-    @Value("${spring.mail.username}")
-    private String fromEmail;
+
+	@Resource
+	private ConfigService configService;
+
+	@Value("${spring.mail.username}")
+	private String fromEmail;
 
 	/**
 	 * 发送验证码到用户邮件
@@ -44,18 +52,29 @@ public class EmailServiceImpl implements EmailService {
 		}
 
 		// 3. 发送邮箱验证码到用户邮箱
-		String emailCode = ToolsUtil.randomCount(100000, 999999).toString();
-		RedisUtils.hset("emailCode", req.getUuid(), emailCode, 300);
+		String systemName = configService.get("systemName");
+		String time = LocalDateTimeUtil.format(LocalDateTime.now(), "yyyy-MM-dd HH:mm:ss");
 
-		SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom(fromEmail);
-        message.setTo(req.getEmail());
-        message.setSubject("邮箱绑定");
-        message.setText("您的验证码是：" + emailCode + "，5分钟内有效。");
-        mailSender.send(message);
+		String htmlTemplate = configService.get("codeHtmlTemplate");
+		try{
+			MimeMessage message = mailSender.createMimeMessage();
+			MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+
+			helper.setFrom(fromEmail);
+			helper.setTo(req.getEmail());
+			helper.setSubject("验证码邮件");
+
+			// 替换占位符并设置 HTML 内容
+			String htmlContent = StrUtil.format(htmlTemplate, systemName, "注册验证码", systemName, systemName, code, fromEmail, systemName, time);
+			helper.setText(htmlContent, true);
+
+			mailSender.send(message);
+		} catch (MessagingException e) {
+			throw new BizException("邮箱发送失败!");
+		}
 
 		// 5. 移除验证码
 		RedisUtils.hdel("numberCode", req.getUuid());
 		return true;
-    }
+	}
 }
